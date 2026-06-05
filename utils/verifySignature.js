9f8fe9fc0ed09e4f1f8571fce939c5a7a542024f
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-06-05 07:46:43 +0530

    verify signature add

---
+++ utils/verifySignature.js
@@ -0,0 +1,23 @@
+import crypto from "crypto";
+
+const WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
+
+export function verifyGithubSignature(req) {
+	const signature = req.headers["x-hub-signature-256"];
+
+	if (!signature) {
+		return false;
+	}
+
+	const expectedSignature =
+		"sha256=" +
+		crypto
+			.createHmac("sha256", WEBHOOK_SECRET)
+			.update(req.rawBody)
+			.digest("hex");
+
+	return crypto.timingSafeEqual(
+		Buffer.from(signature),
+		Buffer.from(expectedSignature),
+	);
+}
