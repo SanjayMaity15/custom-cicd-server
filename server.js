eee4acd96520dc75f3f90af8975ef878ef588a1e
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-06-05 08:12:53 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import { verifyGithubSignature } from "./utils/verifySignature"
+import { verifyGithubSignature } from "./utils/verifySignature.js"
 
 dotenv.config()
 const app = express()
