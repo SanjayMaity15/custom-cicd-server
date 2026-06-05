761d11ccb545c33d529e95ff6c1635adb308021a
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2026-06-05 07:46:26 +0530

    webhook end point add

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { verifyGithubSignature } from "./utils/verifySignature"
 
 dotenv.config()
 const app = express()
@@ -8,10 +9,30 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
+
+app.post("/webhook/github", (req, res) => {
+	const valid = verifyGithubSignature(req);
+
+	if (!valid) {
+		return res.status(401).json({
+			message: "Invalid signature",
+		});
+	}
+
+	console.log("Verified GitHub webhook");
+
+	res.status(200).json({
+		message: "Webhook accepted",
+    });
+    
+    console.log(req.headers)
+    console.log(req.body)
+});
+
 app.get("/", (req, res) => {
     res.send("CI CD SERVER IS RUNNING !!!")
 })
 
 app.listen(PORT, () => {
     console.log("SERVER IS RUNNING AT ", PORT)
-}) 
+})  
